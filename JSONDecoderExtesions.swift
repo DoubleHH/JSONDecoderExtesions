93ce197d8452c54c5a47ff44840d0a4d83f9f6d3
DoubleH <huanghui2008123@gmail.com>
2018-02-07 19:43:26 +0800

    add File

---
+++ JSONDecoderExtesions.swift
@@ -0,0 +1,73 @@
+//
+//  JSONDecoderExtesions.swift
+//
+//  Created by DoubleHH on 2018/2/2.
+//
+
+import Foundation
+
+// MARK: - handle typeMismatch exceptions in JSONDecoder. You can expand the type of you want.
+extension KeyedDecodingContainer {
+    public func decodeIfPresent(_ type: String.Type, forKey key: K) throws -> String? {
+        if let value = try? decode(type, forKey: key) {
+            return value
+        }
+        if let value = try? decode(Int.self, forKey: key) {
+            return String(value)
+        }
+        if let value = try? decode(Float.self, forKey: key) {
+            return String(value)
+        }
+        return nil
+    }
+    
+    public func decodeIfPresent(_ type: Int.Type, forKey key: K) throws -> Int? {
+        if let value = try? decode(type, forKey: key) {
+            return value
+        }
+        if let value = try? decode(String.self, forKey: key) {
+            return Int(value)
+        }
+        return nil
+    }
+    
+    public func decodeIfPresent(_ type: Float.Type, forKey key: K) throws -> Float? {
+        if let value = try? decode(type, forKey: key) {
+            return value
+        }
+        if let value = try? decode(String.self, forKey: key) {
+            return Float(value)
+        }
+        return nil
+    }
+    
+    public func decodeIfPresent(_ type: Bool.Type, forKey key: K) throws -> Bool? {
+        if let value = try? decode(type, forKey: key) {
+            return value
+        }
+        if let value = try? decode(String.self, forKey: key) {
+            if let valueInt = Int(value) {
+                return Bool(valueInt != 0)
+            }
+            return nil
+        }
+        if let value = try? decode(Int.self, forKey: key) {
+            return Bool(value != 0)
+        }
+        return nil
+    }
+    
+    public func decodeIfPresent(_ type: Double.Type, forKey key: K) throws -> Double? {
+        if let value = try? decode(type, forKey: key) {
+            return value
+        }
+        if let value = try? decode(String.self, forKey: key) {
+            return Double(value)
+        }
+        return nil
+    }
+    
+    public func decodeIfPresent<T>(_ type: T.Type, forKey key: K) throws -> T? where T : Decodable {
+        return try? decode(type, forKey: key)
+    }
+}
